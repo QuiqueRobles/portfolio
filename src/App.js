5d3b7f9b65e6ceed29a7a180cff4d6ad44db77c7
QuiqueRobles <e.robles3@usp.ceu.es>
2024-09-02 12:23:55 +0200

    Basic react app working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import './App.css';  // Importa los estilos aquí
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/about">Sobre Mí</Link></li>
+            <li><Link to="/projects">Proyectos</Link></li>
+            <li><Link to="/contact">Contacto</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h2>Contacto</h2>
+      <p>Email: qroblesuriel@gmail.com</p>
+      <p>Teléfono: +34 608 887 536</p>
+    </div>
+  );
+}
+
+export default Contact;
